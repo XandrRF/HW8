f0d85cab5b810e36c40b9626fb6698918b98e118
XandrRF <xandrnf@gmail.com>
2024-12-19 22:39:42 +0500

    надо будет вернутся

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,39 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
-        //todo
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt(100);
+        }
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
-        //todo
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("\n==========================");
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
-        //todo
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
-        //todo
+
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - (1 + i)];
+            array[array.length - (1 + i)] = temp;
+        }
+
+        System.out.println("\n==========================");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+
     }
 }
--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+
+//        Задание
+//        Написать программу, которая сгенерирует случайный вес 10 людям в пределах от 40 до 100 кг.
+//        (Нужно самостоятельно найти информацию, как генерировать числа в заданном диапазоне).
+//
+//        Далее вам нужно будет вычислить несколько показателей:
+//        1. Средний вес всех людей.
+//        2. Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно.
+
+
+        double[] array = new double[10];
+
+        for (int i = 0; i < array.length; i++) {
+
+            array[i] = (new Random().nextDouble() * 60) + 40;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i] + " ");
+        }
+        double weight = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            weight = weight + array[i];
+
+        }
+        System.out.println("Средний вес: " + weight / array.length);
+
+        int cout = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] >= 60 && array[i] <= 80) {
+                ++cout;
+            }
+        }
+         System.out.println("Количество людей у которых вес находится в промежутке от 60 до 80 кг включительно: " + cout);
+
+
     }
 }
